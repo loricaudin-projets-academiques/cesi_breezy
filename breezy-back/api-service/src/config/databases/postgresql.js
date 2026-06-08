8f5b47326423a5fc4459e2bb1432ce4884f43fe2
Loric Audin <loricaudin@yahoo.fr>
2026-06-08 10:51:54 +0200

    Modifs docker compose + package

---
+++ breezy-back/api-service/src/config/databases/postgresql.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  }
+);
+
+console.log(process.env.POSTGRES_DB)
+
+module.exports = sequelize;
--- breezy-back/api-service/src/index.js
+++ breezy-back/api-service/src/index.js
@@ -1,11 +1,33 @@
+
+const path = require("path");
+
+const envPath = path.resolve(__dirname, "../../.env");
+
+require("dotenv").config({ path: envPath });
+
 const express = require('express');
+const sequelize = require("./config/databases/postgresql");
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connexion réussie à la base de données PostgreSQL");
+  } catch (err) {
+    console.error("Erreur de connexion à la base de données PostgreSQL : ", null);
+    return;
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+start();
