1d4e6ac0edb5c69d08096a412647155c67a70149
Loric Audin <loricaudin@yahoo.fr>
2026-06-24 11:00:20 +0200

    1ere phase deplacement

--- api-service/src/services/post/post.service.js
+++
@@ -1,7 +0,0 @@
-async function getPostById(id) {
-    return `Id = ${id}`;
-}
-
-export default {
-    getPostById
-};
--- api-service/src/utils/httpError.js
+++
@@ -1,9 +0,0 @@
-function createHttpError(status, message) {
-  const error = new Error(message);
-  error.status = status;
-  return error;
-}
-
-export {
-  createHttpError,
-};
